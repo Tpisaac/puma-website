4113f82c8baa381ef705c3c7fa12cc48bff088ef
Temitope Ajilore <devtemi@outlook.com>
2023-09-15 14:41:55 +0100

    Updates

---
+++ assets/sh6-6fdf385a.js
@@ -0,0 +1,1 @@
+const s="/puma-website/assets/pumashoe1-ff672e84.png",a="/puma-website/assets/pumashoe2-4d16e456.png",e="/puma-website/assets/pumashoe3-2ebab304.png",p="/puma-website/assets/pumashoe4-d768a566.png",t="/puma-website/assets/sh6-f9e6712a.png";export{a,e as b,p as c,s as p,t as s};
